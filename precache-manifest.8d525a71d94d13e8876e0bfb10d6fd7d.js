ff17806e4854ff18c175e8b2cebfc430d3d18f6a
YassenRabie <YassenRabie@users.noreply.github.com>
2020-10-03 22:18:05 +0300

    Updates

---
+++ precache-manifest.8d525a71d94d13e8876e0bfb10d6fd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dab5f449cc5ce4ae17e6dad4b543278",
+    "url": "/doINeedACat/index.html"
+  },
+  {
+    "revision": "59215d7e87d6f4f63f9f",
+    "url": "/doINeedACat/static/css/main.6fba2456.chunk.css"
+  },
+  {
+    "revision": "95833d2e131e8d85a12d",
+    "url": "/doINeedACat/static/js/2.47445033.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/doINeedACat/static/js/2.47445033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59215d7e87d6f4f63f9f",
+    "url": "/doINeedACat/static/js/main.2e17b51d.chunk.js"
+  },
+  {
+    "revision": "84402a7bb9c0b410e645",
+    "url": "/doINeedACat/static/js/runtime-main.48ebf9b6.js"
+  }
+]);
